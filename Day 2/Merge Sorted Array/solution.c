e886c82c85ed0883e267e493c4e34c30fec2ef1d
Lucas Colas <51634013+LucasColas@users.noreply.github.com>
2022-02-06 14:15:49 +0100

    Create solution.c

---
+++ Day 2/Merge Sorted Array/solution.c
@@ -0,0 +1,28 @@
+
+
+
+void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n){
+    int last = m+n-1;
+    
+    while (m > 0 && n > 0) {
+        if (nums1[m-1] > nums2[n-1]) {
+            nums1[last] = nums1[m-1];
+            m--;
+        }
+        
+        else {
+            nums1[last] = nums2[n-1];
+            n--;
+        }
+        
+        last--;
+    }
+    
+    while (n > 0) {
+        nums1[last] = nums2[n-1];
+        n--;
+        last--;
+    }
+    
+    
+}
