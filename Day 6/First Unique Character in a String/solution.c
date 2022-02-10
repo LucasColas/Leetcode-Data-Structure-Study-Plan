a8a2475e8735411d00dfee99ffd896e93e5dfff6
Lucas Colas <51634013+LucasColas@users.noreply.github.com>
2022-02-10 08:32:32 +0100

    Create solution.c

---
+++ Day 6/First Unique Character in a String/solution.c
@@ -0,0 +1,23 @@
+
+
+int firstUniqChar(char * s){
+    
+    if (s == 1) {
+        return 0;
+    }
+    
+    int hash[26] = {0};
+    
+    for (int i = 0; s[i] != '\0' ; i++) { // '\0' stands for null terminator, abbreviated NUL, is a control character. Tt serves as a reserved character used to signify the end of a string
+        hash[s[i]-'a']++;
+    }
+    
+    for (int i = 0 ; s[i] != '\0';i++) {
+        if (hash[s[i]-'a'] == 1) {
+            return i;
+        }
+    }
+    
+    return -1;
+
+}
