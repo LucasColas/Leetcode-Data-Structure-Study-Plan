1763abaf888f01a84ac32b6fcf6183514e90619d
Lucas Colas <51634013+LucasColas@users.noreply.github.com>
2022-02-14 13:09:04 +0100

    Update solution.c

--- Day 10/Binary Tree Inorder Traversal/solution.c
+++ Day 10/Binary Tree Inorder Traversal/solution.c
@@ -1,1 +1,32 @@
-//
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+void traverse(struct TreeNode* root, int* array, int* index){
+    if(root->left) {
+        traverse(root->left,array,index);   
+    }
+    array[(*index)++]=root->val;
+    if(root->right) {
+        traverse(root->right,array,index);
+    }
+        
+}
+
+int* inorderTraversal(struct TreeNode* root, int* returnSize){
+    int*array=malloc(100*sizeof(int));
+    *returnSize=0;
+    if(root)
+        traverse(root,array,returnSize);
+    //arr=realloc(arr,(*returnSize)*sizeof(int));
+    return array;
+}
