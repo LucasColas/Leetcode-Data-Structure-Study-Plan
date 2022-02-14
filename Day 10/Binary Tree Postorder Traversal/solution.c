7286e3fb8817a3eb281e0100b76cdbe4c83c86e6
Lucas Colas <51634013+LucasColas@users.noreply.github.com>
2022-02-14 13:11:32 +0100

    Create solution.c

---
+++ Day 10/Binary Tree Postorder Traversal/solution.c
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+
+void traverse(struct TreeNode* root, int* array, int* index){
+    if(root->left) {
+        traverse(root->left,array,index);   
+    }
+    
+    if(root->right) {
+        traverse(root->right,array,index);
+    }
+    array[(*index)++] = root->val;
+        
+}
+
+int* postorderTraversal(struct TreeNode* root, int* returnSize){
+    int*array=malloc(100*sizeof(int));
+    *returnSize=0;
+    if(root) {
+        traverse(root,array,returnSize);
+    }
+    //arr=realloc(arr,(*returnSize)*sizeof(int));
+    return array;
+
+}
